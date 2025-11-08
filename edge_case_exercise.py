d4943e34e922f1ee9928c6314d6af0680d5180ac
ariel1301 <ariel.ribo@gmail.com>
2025-11-08 12:56:49 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,22 +1,13 @@
-def move(my_list, direction):
-    # מציאת האינדקס של ה-1 (מניחים שקיים 1 אחד בדיוק לפי ההנחיות)
-    index_of_one = my_list.index(1)
+def move(my_list, direction=None):
+    index_of_one = my_list.index(1) 
     
-    # גודל הרשימה
-    list_length = len(my_list) 
-    
-    # הזזה ימינה
     if direction == 'right':
-        # בודק אם ה-1 אינו האיבר האחרון
-        if index_of_one < list_length - 1:
+        if index_of_one < len(my_list) - 1:
             my_list[index_of_one] = 0
             my_list[index_of_one + 1] = 1
-            
-    # הזזה שמאלה
     elif direction == 'left':
-        # בודק אם ה-1 אינו האיבר הראשון
         if index_of_one > 0:
             my_list[index_of_one] = 0
             my_list[index_of_one - 1] = 1
-            
+
     return my_list
